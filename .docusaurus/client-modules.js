c993b222fa276151b90a8ed3241833081fe22be7
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2022-01-03 23:57:28 +0100

    chore: cleanup

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/kevinmaikjablonka/Dropbox (LSMO)/Documents/open_source/MADICES.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/kevinmaikjablonka/Dropbox (LSMO)/Documents/open_source/MADICES.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/kevinmaikjablonka/Dropbox (LSMO)/Documents/open_source/MADICES.github.io/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/Users/kevinmaikjablonka/Dropbox (LSMO)/Documents/open_source/MADICES.github.io/src/css/custom.css"),
-];
