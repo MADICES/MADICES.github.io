aaa09b81e0700f30768ec242ae4d0dc81b7c6004
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2022-01-14 08:28:53 +0100

    feat: add CoC

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,42 +1,41 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const lightCodeTheme = require("prism-react-renderer/themes/github");
 
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 (
   module.exports = {
-    title: 'MADICES 2022',
+    title: "MADICES 2022",
     tagline:
-      'Machine actionable data for chemical sciences:  Bridging experiments, simulations, and machine learning for spectral data',
-    url: 'https://madices.github.io',
-    baseUrl: '/',
-    favicon: 'img/favicon.ico',
-    organizationName: 'MADICES',
-    projectName: 'madices.github.io',
-    onBrokenLinks: 'throw',
-    onBrokenMarkdownLinks: 'warn',
+      "Machine actionable data for chemical sciences:  Bridging experiments, simulations, and machine learning for spectral data",
+    url: "https://madices.github.io",
+    baseUrl: "/",
+    favicon: "img/favicon.ico",
+    organizationName: "MADICES",
+    projectName: "madices.github.io",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
     presets: [
       [
-        '@docusaurus/preset-classic',
+        "@docusaurus/preset-classic",
         /** @type {import('@docusaurus/preset-classic').Options} */
         ({
           docs: {
-            path: 'docs',
-            sidebarPath: 'sidebars.js',
-            editUrl:
-              'https://github.com/madices/madices.github.io/edit/main/',
+            path: "docs",
+            sidebarPath: "sidebars.js",
+            editUrl: "https://github.com/madices/madices.github.io/edit/main/",
             versions: {
               current: {
-                label: 'current',
+                label: "current",
               },
             },
-            lastVersion: 'current',
+            lastVersion: "current",
             showLastUpdateAuthor: false,
             showLastUpdateTime: true,
           },
           theme: {
-            customCss: require.resolve('./src/css/custom.css'),
+            customCss: require.resolve("./src/css/custom.css"),
           },
         }),
       ],
@@ -48,36 +47,36 @@ const lightCodeTheme = require('prism-react-renderer/themes/github');
         navbar: {
           hideOnScroll: true,
           logo: {
-            alt: 'MADICES',
-            src: 'img/svg/c_no_bg.svg',
-            srcDark: 'img/svg/w_no_bg.svg',
+            alt: "MADICES",
+            src: "img/svg/c_no_bg.svg",
+            srcDark: "img/svg/w_no_bg.svg",
           },
           items: [
             // left
             {
-              label: 'Workshop objectives',
-              to: 'docs/objectives',
-              position: 'left',
+              label: "Workshop objectives",
+              to: "docs/objectives",
+              position: "left",
             },
             {
-              label: 'Program',
-              to: 'docs/program',
-              position: 'left',
+              label: "Program",
+              to: "docs/program",
+              position: "left",
             },
             {
-              label: 'Speakers',
-              to: 'docs/speakers',
-              position: 'left',
+              label: "Speakers",
+              to: "docs/speakers",
+              position: "left",
             },
             {
-              label: 'Organizers',
-              to: 'docs/organizers',
-              position: 'left',
+              label: "Organizers",
+              to: "docs/organizers",
+              position: "left",
             },
           ],
         },
         colorMode: {
-          defaultMode: 'light',
+          defaultMode: "light",
           disableSwitch: false,
           respectPrefersColorScheme: false,
         },
@@ -88,31 +87,35 @@ const lightCodeTheme = require('prism-react-renderer/themes/github');
         footer: {
           links: [
             {
-              title: 'Community',
+              title: "Community",
               items: [
                 {
-                  label: 'Discord',
-                  to: 'https://discord.gg/Fjm9v4rmTW',
+                  label: "Discord",
+                  to: "https://discord.gg/Fjm9v4rmTW",
+                },
+                {
+                  label: "Code of Conduct",
+                  to: "docs/coc",
                 },
               ],
             },
             {
-              title: 'Social',
+              title: "Social",
               items: [
                 {
-                  label: 'GitHub',
-                  to: 'https://github.com/madices/MADICES-2022',
+                  label: "GitHub",
+                  to: "https://github.com/madices/MADICES-2022",
                 },
               ],
             },
           ],
           logo: {
-            alt: 'MADICES',
-            src: 'img/svg/c_no_bg.svg',
+            alt: "MADICES",
+            src: "img/svg/c_no_bg.svg",
           },
-          copyright: 'MADICES 2022',
+          copyright: "MADICES 2022",
         },
-        image: 'img/svg/c_no_bg.svg',
+        image: "img/svg/c_no_bg.svg",
         prism: {
           theme: lightCodeTheme,
           darkTheme: darkCodeTheme,
