2ae4750a40535f8c041e10bccc879dda8427baec
Edan Bainglass <45081142+edan-bainglass@users.noreply.github.com>
2025-03-01 11:29:31 +0100

    Fix sponsor box layout (#38)

--- docs/components.jsx
+++ docs/components.jsx
@@ -2,10 +2,7 @@ import React from "react";
 import classNames from "classnames";
 
 export function Sponsor({ name, link, img, className }) {
-  const classes = classNames(
-    "col-12 col-md-6 d-flex justify-content-center",
-    className
-  );
+  const classes = classNames("col-12 col-md-6 d-flex sponsor-box", className);
 
   return (
     <div className={classes}>
